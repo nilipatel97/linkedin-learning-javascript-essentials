5fcddbe55c086574716ca1deb4dfb1c863648873
Nili Patel <88466745+nilipatel97@users.noreply.github.com>
2022-05-12 16:55:32 -0400

    added house class

--- exercise-ch3-less12/index.js
+++ exercise-ch3-less12/index.js
@@ -1,6 +1,8 @@
 import Backpack from "./Backpack.js";
 import PencilCase from "./PencilCase.js";
-
+import House from "./House.js";
+/******************************************* */
+console.log("********************************")
 const workBackpack = new Backpack(
   "Nili's Work Backpack",
   20,
@@ -13,6 +15,8 @@ const workBackpack = new Backpack(
 
 console.log("This is Nili's work backpack:", workBackpack);
 
+/******************************************* */
+console.log("********************************")
 const workPencilCase = new PencilCase(
   "Nili's Work PencilCase",
   "Nili Patel",
@@ -32,3 +36,14 @@ console.log(
   ": ",
   workPencilCase.calculateVolume()
 );
+
+/******************************************* */
+console.log("********************************")
+const inmanHouse = new House("The Patel Household", "963 Inman Avenue.", 2001, 6000000, 2400, 4, 3, 15000);
+
+console.log("The inmanHouse object:", inmanHouse);
+console.log("House Name: ", inmanHouse.houseName);
+console.log("Cost of House: ", inmanHouse.cost);
+inmanHouse.setCost(8000000);
+console.log("New Cost of House", inmanHouse.cost);
+console.log("Selling Point: ", inmanHouse.enthustiasticPitch());
